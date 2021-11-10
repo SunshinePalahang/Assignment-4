deaac2608d60bee32cb822821d46e753ea68f42e
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-10 21:05:54 +0800

    name, age, add input function

---
+++ program1ver3.py
@@ -0,0 +1,9 @@
+def getNameAgeAdd():
+    _name = input("Enter name: ")
+    _age = input("Enter age: ")
+    _add = input("Enter address: ") 
+    return _name,_age,_add
+
+
+
+name, age, add = getNameAgeAdd()
