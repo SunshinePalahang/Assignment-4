6dba79f1ea28a99310179d75b97aadbeccdb224d
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-10 21:12:33 +0800

    program 1 all user input in one function

--- program1ver3.py
+++ program1ver3.py
@@ -4,6 +4,8 @@ def getNameAgeAdd():
     _add = input("Enter address: ") 
     return _name,_age,_add
 
-
+def display(name, age, add):
+    print(f"Hi, my name is {name}. I am {age} years old and I live in {add}.")
 
 name, age, add = getNameAgeAdd()
+display(name, age, add)
