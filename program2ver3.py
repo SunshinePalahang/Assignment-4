05dba74a575fcca36a50cf3b005e9d6c17b29a19
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-10 21:23:35 +0800

    program 2 all user input in one function

--- program2ver3.py
+++ program2ver3.py
@@ -1,7 +1,11 @@
 def getAppleOrangeQty():
     _appleQty = int(input("How many apple? "))
     _orangeQty =int(input("How many orange? "))
-    return _appleQty,_orangeQty
+    return int(_appleQty*(20) + _orangeQty*(25))
 
+def display(AppleOrangeQty):
+    print (f"The total amount is {AppleOrangeQty}.")
 
 AppleOrangeQty = getAppleOrangeQty()
+
+display(AppleOrangeQty)
